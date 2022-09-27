148db960d1f5d377c4708dc64ce6aebb775d6f5a
Celeo <mattboulanger@fastmail.com>
2022-09-26 21:53:27 -0700

    Start client

---
+++ user_client/src/main.rs
@@ -0,0 +1,41 @@
+use msfs::sim_connect::{
+    data_definition, Period, SimConnect, SimConnectRecv, SIMCONNECT_OBJECT_ID_USER,
+};
+
+#[data_definition]
+#[derive(Debug)]
+struct Data {
+    #[name = "INDICATED ALTITUDE"]
+    #[epsilon = 0.01]
+    altitude: f64,
+    #[name = "AIRSPEED INDICATED"]
+    #[epsilon = 0.01]
+    airspeed: f64,
+    #[name = "PLANE LONGITUDE"]
+    #[unit = "degree longitude"]
+    #[epsilon = 0.01]
+    longitude: f64,
+    #[name = "PLANE LATITUDE"]
+    #[unit = "degree latitude"]
+    #[epsilon = 0.01]
+    latitude: f64,
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let mut sim = SimConnect::open("LOG", |sim, recv| match recv {
+        SimConnectRecv::SimObjectData(event) => match event.dwRequestID {
+            0 => {
+                println!("{:?}", event.into::<Data>(sim).unwrap());
+            }
+            _ => {}
+        },
+        _ => println!("{:?}", recv),
+    })?;
+
+    sim.request_data_on_sim_object::<Data>(0, SIMCONNECT_OBJECT_ID_USER, Period::SimFrame)?;
+
+    loop {
+        sim.call_dispatch()?;
+        std::thread::sleep(std::time::Duration::from_millis(1000));
+    }
+}
